a85e1c5b4e1ab7e8f7fdda62780ab772e040a763
samkc-0 <sam.k.c.0@icloud.com>
2025-09-16 09:44:59 +0100

    add cli arg parsing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 from stats import count_words, count_characters, sort_characters_by_count, format_report
 
@@ -7,7 +8,18 @@ def get_book_text(file_path: Path) -> str:
 
 
 def main():
-    book_path = Path(".") / "books" / "frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    try:
+        book_path = Path(book_path)
+    except ValueError:
+        print(f"Error: {book_path} is not a valid path")
+        sys.exit(1)
+    if not Path(book_path).is_file():
+        print(f"Error: {book_path} is not a file")
+        sys.exit(1)
     text = get_book_text(book_path)
     word_count = count_words(text)
     char_counts = sort_characters_by_count(count_characters(text))
